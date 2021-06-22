b060e24ce80222e8b27751310b4dc5fdbb23b3da
newnewff <39368783+newnewff@users.noreply.github.com>
2021-06-22 09:59:05 +0800

    Add files via upload

---
+++ python/mermaid.py
@@ -0,0 +1,23 @@
+# python 与使用 mermaid.ink 服务的 mermaid-js 集成
+import base64
+import requests, io
+from PIL import Image
+import matplotlib.pyplot as plt
+
+graph = """
+graph LR;
+    A--> B & C & D3;
+    B--> A & E;
+    C--> A & E;
+    D3--> A & E;
+    E--> B & C & D3;
+"""
+
+graphbytes = graph.encode("ascii")
+base64_bytes = base64.b64encode(graphbytes)
+base64_string = base64_bytes.decode("ascii")
+print('https://mermaid.ink/img/' + base64_string)
+#img = Image.open(io.BytesIO(requests.get('https://mermaid.ink/img/' + base64_string).content))
+#plt.imshow(img)
+
+
