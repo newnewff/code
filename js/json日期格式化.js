2ba85b20af652f03b88983e3eee8a01228ce3b99
newnewff <39368783+newnewff@users.noreply.github.com>
2021-06-25 11:25:34 +0800

    Create json日期格式化.js

---
+++ js/json日期格式化.js
@@ -0,0 +1,10 @@
+function ConvertJSONDateToJSDate(jsondate) {
+    var date = new Date(parseInt(jsondate.replace("/Date(", "").replace(")/", ""), 10));
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+    return year + "-" + month + "-" + day + " " + hh + ":" + mm + ":" + ss;
+}
