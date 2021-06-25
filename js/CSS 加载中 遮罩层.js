ad69e6285149bdff6e074fa12d2eb88f74e2d2f7
newnewff <39368783+newnewff@users.noreply.github.com>
2021-06-25 11:33:09 +0800

    Create CSS 加载中 遮罩层.js

---
+++ js/CSS 加载中 遮罩层.js
@@ -0,0 +1,35 @@
+.bg_loading{   
+   position: absolute;top:0;left:0;bottom:0;right:0;width:50%;height:50%;margin:auto;  color:#000000;
+}   
+.bg {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+    outline: 0;
+    -webkit-overflow-scrolling: touch;
+    background-color: rgb(0, 0, 0);  
+    filter: alpha(opacity=10);  
+    background-color: rgba(0, 0, 0, 0.1); 
+    z-index: 9999;
+    text-align:center;
+      margin:0 auto;
+}
+
+  <div class="bg" id="bg" style="display: none;"> <div class="bg_loading">加载中...</div> </div>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+       
+    });
+    $(document).ajaxStart(function () {
+        $("#bg").show();
+    });
+
+    //ajax请求结束时移除loading  
+    $(document).ajaxComplete(function () {
+        $("#bg").hide();
+    });
+</script> 
