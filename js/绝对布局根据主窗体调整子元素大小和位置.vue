838e889ec1b1aded8a723316e3224f946ee3677c
newnewff <39368783+newnewff@users.noreply.github.com>
2024-10-14 15:45:11 +0800

    Update 绝对布局根据主窗体调整子元素大小和位置.vue

--- js/绝对布局根据主窗体调整子元素大小和位置.vue
+++ js/绝对布局根据主窗体调整子元素大小和位置.vue
@@ -4,7 +4,7 @@
       <img v-if="mainImage.url" v-show="domsPos.length==0" style="height:98vh" :src="mainImage.url"/>
       <div v-for="item in domsPos" :style="`width:${}px;height:${}px;left:${}px;top:${}px;zIndex:${};`">
         <img :src="item.url" v-if="item.type=='image'" :style="`width:${}px;height:${}px;`"/>
-        <span v-else-if="item.type=="sensor"></span>
+        <span v-else-if="item.type=='sensor'"></span>
       </div>
     </div>
   </div>
