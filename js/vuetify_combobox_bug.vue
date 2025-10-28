36149ce32a92684f6b88fb507b622478dd64ca15
newnewff <39368783+newnewff@users.noreply.github.com>
2025-10-28 15:37:09 +0800

    Create vuetify_combobox_bug.vue

---
+++ js/vuetify_combobox_bug.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<v-combobox v-model="model"  :items="items" label="Search for an option" multiple >
+		    <template v-slot:item="{ props, item }">
+		       <v-list-item  title="Item is already selected" >
+  					<template v-slot:default>
+  						{{item.value}}
+  					  <v-checkbox :label="item.value" :value="item.value"></v-checkbox>
+  					</template>
+				  </v-list-item>
+		    </template>
+		</v-combobox>
+		
+	</div>
+</template>
+
+<script setup>
+  const items = reactive([1,2,3,4])
+	const model = ref([])
+</script>
